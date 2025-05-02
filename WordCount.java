1216237e791f25198c42a8feea071b2cd0bc05a3
shivtej <shivtej2904@gmail.com>
2025-05-02 18:40:07 +0530

    uploaded all codes

---
+++ WordCount.java
@@ -0,0 +1,11 @@
+import org.apache.hadoop.mapreduce.*;
+
+public class WordCount{
+    public static class Map extends Mapper<object,Text,Text,IntWritable>{
+
+    }
+}
+
+public static void main(String[] args) {
+    
+}
